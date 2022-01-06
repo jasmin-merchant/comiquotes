2e8a3597d0cf39103642b2e0c7e4b022c232654b
Jasmin Merchant <jasmin-merchant>
2022-01-06 22:54:39 +0530

    Added comiquotes link

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl: "https://comiquotes.gatsbyjs.io",
     title: "ComiQuotes",
   },
   plugins: ["gatsby-plugin-postcss"],
