35a9b9ba441fb8fb9281746dcce9505518af945a
Jasmin Merchant <jasmin-merchant>
2022-01-08 02:55:19 +0530

    Added Header Component

---
+++ src/components/logo.component.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Logo = () => {
+    return (
+        <div className="text-xl font-audiowide tracking-wider">
+            <div className="logo-comi inline-block p-3 pl-6 pr-1 text-white dark:text-stone-900 bg-stone-900 dark:bg-white border-4 border-r-0 border-stone-900 dark:border-white">Comi</div>
+            <div className='quotes inline-block p-3 pl-1 pr-6 bg-white dark:bg-red-900 text-red-900 dark:text-white border-4 border-l-0 border-red-900 '>Quotes</div>
+        </div>
+    )
+}
+
+export default Logo;
