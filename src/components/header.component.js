7d5556c5b0e8236eef15b8fc478ce74f0c16d0f7
Jasmin Merchant <54054636+jasmin-merchant@users.noreply.github.com>
2022-01-08 03:02:28 +0530

    Add header component (#5)
    * Added Header Component
    
    * Added unit test case for the header
    
    Co-authored-by: Jasmin Merchant <jasmin-merchant>

---
+++ src/components/header.component.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Logo from './logo.component'
+
+const Header = () => (
+    <header className='p-4 bg-transparent fixed'>
+        <Logo />
+    </header>
+)
+
+export default Header
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,20 @@
-import * as React from "react"
+import * as React from "react";
+import Header from "../components/header.component";
 
 const IndexPage = () => {
   return (
-    <main className="container-xl px-24 h-screen flex justify-items-center items-center">
-      <title>ComiQuotes</title>
-      <div className="flex-1">
-        <h1 className="text-5xl text-center font-audiowide"><span className="text-red-900">Comi</span>Quotes</h1>  
-      </div>
-    </main>
-  )
-}
+    <div>
+      <Header />
+      <main className="container-xl px-24 h-screen flex justify-items-center items-center">
+        <title>ComiQuotes</title>
+        <div className="flex-1">
+          <h1 className="text-5xl text-center font-audiowide">
+            <span className="text-red-900">Comi</span>Quotes
+          </h1>
+        </div>
+      </main>
+    </div>
+  );
+};
 
-export default IndexPage
+export default IndexPage;
