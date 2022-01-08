94b827d12c31c409a6a415962bea41293fd53767
Jasmin Merchant <jasmin-merchant>
2022-01-08 15:51:02 +0530

    Added Universe Component with logos

--- src/components/header.component.js
+++ src/components/header.component.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Logo from './logo.component'
 
 const Header = () => (
-    <header className='p-4 bg-transparent fixed'>
+    <header className='p-4 bg-transparent fixed z-10'>
         <Logo />
     </header>
 )
