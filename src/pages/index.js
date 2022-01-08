c0319ac7a4b22dedc241b8f063ea799c214a8e10
Jasmin Merchant <jasmin-merchant>
2022-01-08 21:52:50 +0530

    Added dark toggle theme

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,31 @@
-import * as React from "react";
-import Header from "../components/header.component";
+import React, { useState } from "react";
 import UniverseSection from "../components/universe-section.component";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons";
+
+import Logo from "../components/logo.component";
+
 const IndexPage = () => {
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
   return (
-    <div className="dark dark:bg-stone-900">
-      <Header />
+    <div className={`dark:bg-stone-900 ${isDarkTheme ? "dark" : ""}`}>
+      <header className="w-full p-4 bg-transparent flex justify-between items-center fixed z-10">
+        <Logo />
+        <div>
+          <FontAwesomeIcon
+            icon={isDarkTheme ? faSun : faMoon}
+            className="text-stone-900 dark:text-yellow-400 text-xl hover:cursor-pointer"
+            onClick={() => setIsDarkTheme(!isDarkTheme)}
+          />
+        </div>
+      </header>
       <main className="">
         <title>ComiQuotes</title>
         <section className="flex flex-col lg:flex-row">
-            <UniverseSection type="dcu" />
-            <UniverseSection type="mcu" />
+          <UniverseSection type="dcu" />
+          <UniverseSection type="mcu" />
         </section>
       </main>
     </div>
