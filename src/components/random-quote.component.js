f9621571f495922f648b45f51373a562fb7e977f
Jasmin Merchant <jasmin-merchant>
2022-01-08 19:58:05 +0530

    Added Quote component, copy functionality & used state for quotes

---
+++ src/components/random-quote.component.js
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCopy, faSyncAlt } from "@fortawesome/free-solid-svg-icons";
+
+const RandomQuote = ({ type }) => {
+  const [randomQuote, setRandomQuote] = useState({});
+
+  //   useEffect(() => {
+  //       fetch("https://akabab.github.io/superhero-api/api/all.json")
+  //         .then(response => response.json())
+  //         .then(result => setRandomQuote(response))
+  //         .catch(error => console.log('error', error));
+  //   }, []);
+
+  useEffect(() => {
+    if (type === "dcu") {
+      const randomQuoteVal = {
+        id: "BJBmaixf27TepVXj55Qnf6",
+        data: {
+          author: "Harley Quinn",
+          quote:
+            "Every woman has a crazy side that only the right man can bring out.",
+        },
+      };
+      setRandomQuote(randomQuoteVal);
+    } else {
+      const randomQuoteVal = {
+        id: "HFTmV9cxK6u2yLanUesRai",
+        data: {
+          author: "Iron Man",
+          quote:
+            "You know what, give me a break, Steve. I just got hit in the head with a Hulk.",
+        },
+      };
+      setRandomQuote(randomQuoteVal);
+    }
+  }, [type]);
+
+  console.log(randomQuote.data.quote);
+
+  return (
+    <div className="font-audiowide w-3/5 mx-auto dark:text-white">
+      {randomQuote && randomQuote.data  ? (
+        <div>
+          <div>
+            <p className="quote text-center">{randomQuote.data.quote}</p>
+            <p className="author text-right">- {randomQuote.data.author}</p>
+          </div>
+          <div className="action-icons text-center">
+            <FontAwesomeIcon
+              className="dark:text-white mr-4 hover:cursor-pointer"
+              icon={faCopy}
+              onClick={() => {
+                navigator.clipboard.writeText(
+                  randomQuote.data.quote
+                );
+              }}
+            />
+            <FontAwesomeIcon
+              className="dark:text-white hover:cursor-pointer"
+              icon={faSyncAlt}
+            />
+          </div>
+        </div>
+      ) : (
+        <div>No Quotes</div>
+      )}
+    </div>
+  );
+};
+
+export default RandomQuote;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      height: theme => ({
+        "screen/2": "50vh",
+        "screen/3": "calc(100vh / 3)",
+        "screen/4": "calc(100vh / 4)",
+        "screen/5": "calc(100vh / 5)",
+      }),
+
+    },
     fontFamily: {
       'audiowide': ['Audiowide', 'cursive'],
       'quicksand': ['Quicksand', 'sans-serif']
