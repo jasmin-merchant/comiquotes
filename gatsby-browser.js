7db8fe1137a28d2ec2a15b09524dab33172c6e29
Jasmin Merchant <54054636+jasmin-merchant@users.noreply.github.com>
2022-01-09 19:08:26 +0530

    Added GIF loader while loading quotes (#15)

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,14 @@
 import "./src/css/index.css"
+
+const addScript = url => {
+    const script = document.createElement("script")
+    script.src = url
+    script.async = true
+    document.body.appendChild(script)
+  }
+  
+  export const onClientEntry = () => {
+    window.onload = () => {
+      addScript("https://tenor.com/embed.js")
+    }
+  }
